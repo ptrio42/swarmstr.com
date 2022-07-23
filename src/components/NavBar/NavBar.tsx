6eeafda0689a49ae9d729cfb2cdbae6a863e967f
ptrio42 <piotr@entereighth.com>
2022-07-23 23:48:31 +0200

    closes #6

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,17 +7,110 @@ import Grid from '@mui/material/Grid';
 import {Link} from "react-router-dom";
 import Button from '@mui/material/Button';
 import Receipt from '@mui/icons-material/Receipt';
+import Media from 'react-media';
+import Menu from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Drawer from '@mui/material/Drawer';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 export const NavBar = () => {
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+            event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' ||
+                (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
+        setState(open);
+    };
+
+    const list = () => (
+        <Box
+            sx={{ width: 'auto' }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                <ListItem disablePadding component={Link} to="/#were-handed-a-card" sx={{ color: '#000' }}>
+                    <ListItemButton>
+                        <ListItemText sx={{ textTransform: 'uppercase' }} primary="Got a card?" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding component={Link} to="/#let-s-shame-someone"  sx={{ color: '#000' }}>
+                    <ListItemButton>
+                        <ListItemText sx={{ textTransform: 'uppercase' }} primary="Shame someone" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding component={Link} to="/#credits" sx={{ color: '#000' }}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Receipt />
+                        </ListItemIcon>
+                        <ListItemText sx={{ textTransform: 'uppercase' }} primary="Credits" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar style={{ background: '#F0E68C' }} position="static">
                 <Toolbar>
                     <Avatar style={{ border: '3px solid #fff' }} alt="Useless Shit" src={process.env.PUBLIC_URL + '/images/uselessshit-logo.png'} />
                     <Grid container justifyContent="flex-end" sx={{ color: '#000' }}>
-                        <Button sx={{ fontWeight: 'bold' }} variant="text" color="inherit" component={Link} to="/#were-handed-a-card">Got a card?</Button>
-                        <Button sx={{ fontWeight: 'bold' }} variant="text" color="inherit" component={Link} to="/#let-s-shame-someone">Shame someone</Button>
-                        <Button sx={{ fontWeight: 'bold' }} variant="text" color="inherit" component={Link} to="/#credits" startIcon={<Receipt />}>Credits</Button>
+                        <Media query={{ maxWidth: '422px' }} render={() => (
+                            <React.Fragment>
+                                <IconButton onClick={toggleDrawer(true)}><Menu /></IconButton>
+                                <Drawer
+                                    anchor="right"
+                                    open={state}
+                                    onClose={toggleDrawer(false)}
+                                >
+                                    {list()}
+                                </Drawer>
+                            </React.Fragment>
+                        )} />
+                        <Media query={{ minWidth: '423px' }} render={() => (
+                            <React.Fragment>
+                                <Button
+                                    sx={{ fontWeight: 'bold' }}
+                                    variant="text"
+                                    color="inherit"
+                                    component={Link}
+                                    to="/#were-handed-a-card"
+                                >
+                                    Got a card?
+                                </Button>
+                                <Button
+                                    sx={{ fontWeight: 'bold' }}
+                                    variant="text"
+                                    color="inherit"
+                                    component={Link}
+                                    to="/#let-s-shame-someone"
+                                >
+                                    Shame someone
+                                </Button>
+                                <Button
+                                    sx={{ fontWeight: 'bold' }}
+                                    variant="text"
+                                    color="inherit"
+                                    component={Link}
+                                    to="/#credits"
+                                    startIcon={<Receipt />}
+                                >
+                                    Credits
+                                </Button>
+                            </React.Fragment>
+                        )} />
                     </Grid>
                 </Toolbar>
             </AppBar>
