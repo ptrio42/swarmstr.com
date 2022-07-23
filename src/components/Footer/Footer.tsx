f6c01e70befba7b6ea53ad77110d72d2c15ab1b3
ptrio42 <piotr@entereighth.com>
2022-07-23 23:48:31 +0200

    closes #8

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,29 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import styled from "@mui/material/styles/styled";
+import Favorite from '@mui/icons-material/Favorite';
+import Twitter from '@mui/icons-material/Twitter';
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body1,
+    textAlign: 'center',
+    background: 'transparent',
+    color: '#C0C0C0'
+}));
 
 export const Footer = () => {
     return (
-        <Grid container justifyContent="center" alignContent="center" sx={{ width: '100%', height: '133px', background: '#000', color: '#C0C0C0' }}>
-            &copy; 2022 UselessShit.co | &nbsp;<Link href="https://twitter.com/pitiunited" color="inherit">Get in touch</Link>
+        <Grid container direction="column" justifyContent="center" alignContent="center" sx={{ width: '100%', height: '133px', background: '#000', color: '#C0C0C0' }}>
+            <Item>
+                &copy; 2022 UselessShit.co
+            </Item>
+            <Grid item>
+                <Item sx={{ display: 'inline-flex' }}>
+                    Made with <Favorite sx={{ color: '#FFA500', margin: 'auto 0.25em' }} /> in Warsaw by <Twitter sx={{ color: '#1976D2', margin: 'auto 0.25em' }} /> <Link href="https://twitter.com/pitiunited" target="_blank">pitiunited</Link>
+                </Item>
+            </Grid>
         </Grid>
     );
 };
