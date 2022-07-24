7d0777dfccbde270ff1d7c1dfcd76434c9ce1159
ptrio42 <piotr@entereighth.com>
2022-07-24 15:16:00 +0200

    closes #14

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import Paper from '@mui/material/Paper';
 import styled from "@mui/material/styles/styled";
 import Favorite from '@mui/icons-material/Favorite';
 import Twitter from '@mui/icons-material/Twitter';
+import GitHub from '@mui/icons-material/GitHub';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body1,
@@ -15,13 +16,18 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export const Footer = () => {
     return (
-        <Grid container direction="column" justifyContent="center" alignContent="center" sx={{ width: '100%', height: '133px', background: '#000', color: '#C0C0C0' }}>
+        <Grid container direction="column" justifyContent="center" alignContent="center" sx={{ width: '100%', height: '153px', background: '#000', color: '#C0C0C0' }}>
             <Item>
                 &copy; 2022 UselessShit.co |&nbsp; <a className="link1" href="https://opensource.org/licenses/MIT" target="_blank">License</a>
             </Item>
             <Grid item>
                 <Item sx={{ display: 'inline-flex' }}>
                     Made with <Favorite sx={{ color: '#FFA500', margin: 'auto 0.25em' }} /> in Warsaw by <Twitter sx={{ color: '#1976D2', margin: 'auto 0.25em' }} /> <Link href="https://twitter.com/pitiunited" target="_blank">pitiunited</Link>
+                </Item>
+            </Grid>
+            <Grid item>
+                <Item sx={{ display: 'inline-flex', margin: '1em 0' }}>
+                    Check out our <GitHub sx={{ margin: 'auto 0.25em' }} /> <a className="link1" href="https://github.com/ptrio42/uselessshit.co" target="_blank">repository</a>.
                 </Item>
             </Grid>
         </Grid>
