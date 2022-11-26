59b511dadc8bd6366152ed69046f7cbb810c13b0
ptrio42 <piotr@entereighth.com>
2022-11-26 21:10:52 +0100

    feat: Add lnurl to pay invoice

--- src/components/LightningGift/LightningGift.tsx
+++ src/components/LightningGift/LightningGift.tsx
@@ -1,7 +1,7 @@
 import {Box} from "@mui/material";
 import Button from "@mui/material/Button";
 import {createLightningGift} from "../../services/lightningGift";
-import {useEffect, useState} from "react";
+import {MouseEventHandler, useEffect, useState} from "react";
 import * as React from "react";
 import QRCode from 'react-qr-code';
 import Dialog from "@mui/material/Dialog";
@@ -12,6 +12,13 @@ import Tab from "@mui/material/Tab";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from '@mui/icons-material/Close';
 import Pagination from "@mui/material/Pagination";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import styled from "@mui/material/styles/styled";
+import Snackbar from "@mui/material/Snackbar";
+import './LightningGift.css';
+import makeStyles from "@mui/material/styles/makeStyles";
 
 interface LnurlsProps {
     redeem: string;
@@ -31,10 +38,19 @@ interface LightningGiftDialogProps {
     onClose: (value?: string) => void;
 }
 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    color: theme.palette.text.secondary,
+    boxShadow: 'none',
+    maxWidth: '257px'
+}));
+
 export const LightningGiftDialog = ({ open, lnurls, onClose }: LightningGiftDialogProps) => {
     const [tab, setTab] = useState(0);
     const [page, setPage] = useState(1);
     const itemsPerPage = 1;
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setPage(value);
@@ -46,6 +62,21 @@ export const LightningGiftDialog = ({ open, lnurls, onClose }: LightningGiftDial
 
     const handleClose = () => {
         onClose();
+    };
+
+    const copyTextToClipboard = async (text: string) => {
+        if ('clipboard' in navigator) {
+            return await navigator.clipboard.writeText(text);
+        } else {
+            return document.execCommand('copy', true, text);
+        }
+    };
+
+    const handleCopyLnurlClick = (lnurl: string) => {
+        copyTextToClipboard(lnurl)
+            .then(() => {
+                setSnackbarOpen(true);
+            });
     };
 
     return (
@@ -80,7 +111,20 @@ export const LightningGiftDialog = ({ open, lnurls, onClose }: LightningGiftDial
             <TabPanel index={0} value={tab}>
                 {
                     lnurls.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((url: any) => (
-                        <QRCode value={url.pay} />
+                        <Stack spacing={2}>
+                            <Item>
+                                To load up the card with sats, copy the Lightning Invoice by clicking on the box below
+                                or scan the QR Code with a Lightning Wallet.
+                            </Item>
+                            <Item>
+                                <TextField type="text" value={url.pay} inputProps={{ readOnly: true }} onClick={(event) => {
+                                handleCopyLnurlClick((event.target as HTMLInputElement).value)
+                            }} />
+                            </Item>
+                            <Item>
+                                <QRCode value={url.pay} />
+                            </Item>
+                        </Stack>
                     ))
                 }
                 <Pagination className="pagination" count={Math.ceil(lnurls.length / itemsPerPage)} page={page} onChange={handlePageChange} />
@@ -88,11 +132,28 @@ export const LightningGiftDialog = ({ open, lnurls, onClose }: LightningGiftDial
             <TabPanel index={1} value={tab}>
                 {
                     lnurls.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((url: any) => (
-                        <QRCode value={url.redeem} />
+                        <Stack spacing={2}>
+                            <Item>
+                                <TextField type="text" value={url.redeem} inputProps={{ readOnly: true }} onClick={(event) => {
+                                    handleCopyLnurlClick((event.target as HTMLInputElement).value)
+                                }} />
+                            </Item>
+                            <Item>
+                                <QRCode value={url.redeem} />
+                            </Item>
+                        </Stack>
                     ))
                 }
                 <Pagination className="pagination" count={Math.ceil(lnurls.length / itemsPerPage)} page={page} onChange={handlePageChange} />
             </TabPanel>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setSnackbarOpen(false);
+                }}
+                message="Text copied!"
+            />
         </Dialog>
     );
 };
