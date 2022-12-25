444a63c5c24786a7e39956c86df05f146969388a
ptrio42 <piotr@entereighth.com>
2022-12-25 18:47:06 +0100

    update config

--- src/resources/Config.ts
+++ src/resources/Config.ts
@@ -1,7 +1,9 @@
 const API_COINGECKO_BASE_URL = 'https://api.coingecko.com/api/v3';
 const API_LIGHTNING_GIFTS_BASE_URL = 'https://api.lightning.gifts';
+const API_BLOCKCHAIN_INFO_BASE_URL = 'https://blockchain.info';
 
 export const Config = {
   API_COINGECKO_BASE_URL,
-  API_LIGHTNING_GIFTS_BASE_URL
+  API_LIGHTNING_GIFTS_BASE_URL,
+  API_BLOCKCHAIN_INFO_BASE_URL
 };
