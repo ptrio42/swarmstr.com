0f084b0a15d9e68c16876cfe3d96b35f257d2814
ptrio42 <piotr@entereighth.com>
2023-01-05 06:11:12 +0100

    update tip jars

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,6 +8,7 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import Card from "@mui/material/Card";
+import Backdrop from "@mui/material/Backdrop";
 
 export enum CardType {
     BusinessCard = 'business-card',
@@ -69,101 +70,108 @@ export const SocialCard = ({ slogan, sloganColor, sloganFontSize, sloganTextShad
 
     return (
         <React.Fragment>
-            <Card sx={{
-                marginTop: '2em !important',
-                width: `${cardWidth}in`,
-                height: `${cardHeight}in`,
-                margin: '0 auto 3em auto',
-                background: backgroundImage ? `url(${backgroundImage})` : 'none',
-                backgroundSize: getCardPreviewBackgroundSize(),
-                backgroundRepeat: 'no-repeat',
-                backgroundPositionY: type === CardType.Bookmark ? '2in' : '0',
-                backgroundPosition: `${-backgroundPositionX}px ${-backgroundPositionY}px`,
-                borderRadius: '0px'
-            }}>
-                <CardActionArea sx={{
-                    width: overlay ? '90%' : '100%',
-                    height: overlay ? '90%' : '100%',
-                    background: overlay ? overlayColor : 'transparent',
-                    margin: 'auto',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: type === CardType.BusinessCard ? 'center' : 'flex-start' }}
-                >
-                    <Box sx={{ display: 'flex', justifyContent: type === CardType.BusinessCard ? 'center' : 'flex-start' }}>
-                        <CardMedia
-                            component="img"
-                            sx={{
-                                width: `${primaryImageFormatWidth}in`,
-                                height: `${primaryImageFormatHeight}in`,
-                                objectFit: 'fill',
-                                marginTop: '0.15in',
-                                borderRadius: '50%'
-                            }}
-                            image={mainImage}
-                        />
-                        {
-                            (includeLightningGift || (type === CardType.Sticker && lnurl !== '')) &&
-                            <Box
+            <Backdrop
+                sx={{ color: '#fff', background: '#000', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={true}
+            >
+                <Card sx={{
+                    marginTop: '2em !important',
+                    width: `${cardWidth}in`,
+                    height: `${cardHeight}in`,
+                    margin: '0 auto 3em auto',
+                    background: backgroundImage ? `url(${backgroundImage})` : 'none',
+                    backgroundSize: getCardPreviewBackgroundSize(),
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPositionY: type === CardType.Bookmark ? '2in' : '0',
+                    backgroundPosition: `${-backgroundPositionX}px ${-backgroundPositionY}px`,
+                    borderRadius: '33px',
+                }}>
+                    <CardActionArea sx={{
+                        width: overlay ? '90%' : '100%',
+                        height: overlay ? '90%' : '100%',
+                        background: overlay ? overlayColor : 'transparent',
+                        margin: 'auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: type === CardType.BusinessCard ? 'center' : 'flex-start' }}
+                    >
+                        <Box sx={{ display: 'flex', justifyContent: type === CardType.BusinessCard ? 'center' : 'flex-start' }}>
+                            <CardMedia
+                                component="img"
                                 sx={{
-                                    width: `${secondaryImageFormatWidth}in`,
-                                    height: `${secondaryImageFormatHeight}in`,
-                                    marginLeft: '0.1in',
+                                    width: `${primaryImageFormatWidth}in`,
+                                    height: `${primaryImageFormatHeight}in`,
+                                    objectFit: 'fill',
                                     marginTop: '0.15in',
-                                    overflow: 'hidden'
+                                    borderRadius: '50%',
+                                    border: '4px solid #000'
                                 }}
-                            >
+                                image={mainImage}
+                            />
+                            {
+                                (includeLightningGift || (type === CardType.Sticker && lnurl !== '')) &&
                                 <Box
                                     sx={{
                                         width: `${secondaryImageFormatWidth}in`,
                                         height: `${secondaryImageFormatHeight}in`,
-                                        margin: '0',
-                                        padding: '0'
+                                        marginLeft: '0.1in',
+                                        marginTop: '0.15in',
+                                        overflow: 'hidden',
+                                        border: '4px solid #000'
                                     }}
-                                    ref={qrCodeRef}
                                 >
-                                    <QRCode size={qrCodeSize} value={lnurl} />
+                                    <Box
+                                        sx={{
+                                            width: `${secondaryImageFormatWidth}in`,
+                                            height: `${secondaryImageFormatHeight}in`,
+                                            margin: '0',
+                                            padding: '0'
+                                        }}
+                                        ref={qrCodeRef}
+                                    >
+                                        <QRCode size={qrCodeSize} value={lnurl} />
+                                    </Box>
                                 </Box>
-                            </Box>
 
-                        }
-                        { latestBlock &&
-                        <Box sx={{
-                            position: 'absolute',
-                            top: '0.05in',
-                            left: '0.05in',
-                            fontWeight: 'bold',
-                            color: latestBlockColor
-                        }}>
-                            <LatestBitcoinBlock />
+                            }
+                            { latestBlock &&
+                            <Box sx={{
+                                position: 'absolute',
+                                top: '0.05in',
+                                left: '0.05in',
+                                fontWeight: 'bold',
+                                color: latestBlockColor
+                            }}>
+                                <LatestBitcoinBlock />
+                            </Box>
+                            }
                         </Box>
-                        }
-                    </Box>
-                    <CardContent sx={{ padding: '0px', paddingTop: '8px' }}>
-                        <Typography
-                            sx={{
-                                fontSize: `${sloganFontSize}pt`,
-                                color: sloganColor,
-                                maxWidth: `${cardWidth - 0.5}in`,
-                                overflow: 'hidden', overflowWrap: 'break-word',
-                                textShadow: sloganTextShadow ? `1px 1px ${sloganTextShadowColor}` : 'none',
-                                lineHeight,
-                                padding: '8px'
-                            }}
-                            gutterBottom
-                            variant="h5"
-                            component="div"
-                        >
-                            {slogan}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Typography sx={{ fontSize: `${footerFontSize}pt`, color: footerColor }}>
-                            { footer }
-                        </Typography>
-                    </CardActions>
-                </CardActionArea>
-            </Card>
+                        <CardContent sx={{ padding: '0px', paddingTop: '8px' }}>
+                            <Typography
+                                sx={{
+                                    fontSize: `${sloganFontSize}pt`,
+                                    color: sloganColor,
+                                    maxWidth: `${cardWidth - 0.5}in`,
+                                    overflow: 'hidden', overflowWrap: 'break-word',
+                                    textShadow: sloganTextShadow ? `1px 1px ${sloganTextShadowColor}` : 'none',
+                                    lineHeight,
+                                    padding: '8px'
+                                }}
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                            >
+                                {slogan}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Typography sx={{ fontSize: `${footerFontSize}pt`, color: footerColor, marginTop: '-1em' }}>
+                                { footer }
+                            </Typography>
+                        </CardActions>
+                    </CardActionArea>
+                </Card>
+            </Backdrop>
         </React.Fragment>
     );
 };
