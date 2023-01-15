388da97dc57738868a134ec885ff621d1d05b1a9
ptrio42 <piotr@entereighth.com>
2023-01-15 17:59:36 +0100

    update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,31 @@
+import {useEffect, useState} from "react";
+
 export const matchString = (searchString: string, phrase: string) => {
   const regEx = new RegExp(searchString.toLowerCase(), 'g');
   return phrase
       .toLowerCase()
       .match(regEx);
+};
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+};
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
 };
