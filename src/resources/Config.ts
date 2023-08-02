89a68aabd04f3986552ca8711c58da36d870ac89
ptrio42 <piotr@entereighth.com>
2023-08-02 14:37:10 +0200

    update relay urls

--- src/resources/Config.ts
+++ src/resources/Config.ts
@@ -11,44 +11,46 @@ export const Config = {
   NOSTR_CLIENT: {
     MIN_RELAYS: 3,
     RELAY_TIMEOUT: 10000,
-    DEFAULT_RELAYS: [
-      // 'wss://purplepag.es',
-      'wss://nos.lol',
-      'wss://relay.f7z.io',
-      'wss://relay.damus.io',
-      'wss://nostr.mom',
-      'wss://relay.nostr.band',
-      // 'wss://nostr.terminus.money',
-      // 'wss://atlas.nostr.land/',
-      'wss://offchain.pub/',
-      // 'wss://nostr.uselessshit.co',
-      // 'wss://relay.plebstr.com',
-      // 'wss://relay.primal.net',
-      // 'wss://relay.snort.social',
-      // 'wss://nostr-pub.wellorder.net',
-      // 'wss://blastr.f7z.xyz',
-      'wss://nostr.wine',
-      // 'wss://eden.nostr.land',
-      // 'wss://nostr.milou.lol',
-      // 'wss://nostr.fmt.wiz.biz',
-      // 'wss://relay.nostr.info',
-      // 'wss://brb.io',
-      // 'wss://relay.current.fyi',
-      // 'wss://nostr.relayer.se',
-      'wss://nostr.bitcoiner.social',
-      // 'wss://nostr.zebedee.cloud',
-      // 'wss://relay.nostr.bg',
-      // 'wss://nostr.mutinywallet.com',
-
-    ]
   },
   APP_TITLE: 'Swarmstr - Your knowledge hub for curious minds',
   APP_DESCRIPTION: 'Swarmstr is a simple Q&A web-client, powered by #nostr, where users tap into collective wisdom. Ask questions, find answers, and enjoy expert insights. ',
   APP_IMAGE: `${process.env.BASE_URL}/images/swarmstr-cover-2.jpeg`,
-  APP_KEYWORDS: 'nostr, questions, answers, q&a, nostr client',
+  APP_KEYWORDS: 'nostr, asknostr, questions, answers, q&a, nostr clients, zaps, relays, nips, bitcoin, lightning',
   HASHTAG: 'asknostr',
   SEARCH_RELAY: `wss://search.swarmstr.com`,
-  SEARCH_RELAY_PUBLISH: `wss://search.swarmstr.com?api_key=${process.env.SEARCHNOS_API_KEY}`
+  SEARCH_RELAY_PUBLISH: `wss://search.swarmstr.com?api_key=${process.env.SEARCHNOS_API_KEY}`,
+  SERVER_RELAYS: [
+    'wss://nos.lol',
+    'wss://relay.f7z.io',
+    'wss://relay.damus.io',
+    'wss://nostr.mom',
+    'wss://relay.nostr.band',
+    'wss://offchain.pub/',
+    'wss://nostr.uselessshit.co',
+    'wss://relay.plebstr.com',
+    'wss://relay.snort.social',
+    'wss://nostr-pub.wellorder.net',
+    'wss://blastr.f7z.xyz',
+    'wss://nostr.wine',
+    'wss://eden.nostr.land',
+    'wss://nostr.milou.lol',
+    'wss://relay.current.fyi',
+    'wss://nostr.bitcoiner.social',
+    'wss://relay.nostr.bg',
+    'wss://nostr.mutinywallet.com',
+    'wss://purplepag.es'
+  ],
+  CLIENT_RELAYS: [
+    'wss://nos.lol',
+    'wss://relay.f7z.io',
+    'wss://relay.damus.io',
+    'wss://nostr.mom',
+    'wss://relay.nostr.band',
+    'wss://offchain.pub/',
+    'wss://nostr.wine',
+    'wss://nostr.bitcoiner.social'
+  ]
 };
 
-export const DEFAULT_RELAYS = [...Config.NOSTR_CLIENT.DEFAULT_RELAYS];
+export const CLIENT_RELAYS = [...Config.CLIENT_RELAYS];
+export const SERVER_RELAYS = [...Config.SERVER_RELAYS];
